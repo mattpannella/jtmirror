36260d70edb7f464671529cd80061a015faceb4c
mattpannella <mattpannella@gmail.com>
2022-12-02 22:56:41 -0500

    Update test.php

--- .github/test.php
+++ .github/test.php
@@ -6,13 +6,23 @@ $dir = '.';
 $subdirectories = glob($dir . '/Cores/*', GLOB_ONLYDIR);
 foreach ($subdirectories as $subdirectory) {
     $coreName = basename($subdirectory);
-    echo "Zipping {$coreName}";
-    shell_exec("zip {$coreName}.zip Cores/{$coreName}/*");
-    shell_exec("zip -r {$coreName}.zip Presets/{$coreName}/*");
+    echo "Zipping {$coreName}" . PHP_EOL;
+    $command = "zip {$coreName}.zip Cores/{$coreName}/*";
+    echo $command . PHP_EOL;
+    echo shell_exec($command);
+    $command = "zip -r {$coreName}.zip Presets/{$coreName}/*";
+    echo $command . PHP_EOL;
+    echo shell_exec($command);
     $names = explode('.', $coreName);
     $core = $names[1];
-    shell_exec("zip {$coreName}.zip Platforms/{$core}.json");
-    shell_exec("zip {$coreName}.zip Platforms/_images/{$core}.bin");
-    shell_exec("zip -f {$coreName}.zip Assets/{$core}/*");
+    $command = "zip {$coreName}.zip Platforms/{$core}.json";
+    echo $command . PHP_EOL;
+    echo shell_exec($command);
+    $command = "zip {$coreName}.zip Platforms/_images/{$core}.bin";
+    echo $command . PHP_EOL;
+    echo shell_exec($command);
+    $command = "zip -f {$coreName}.zip Assets/{$core}/*";
+    echo $command . PHP_EOL;
+    echo shell_exec($command);
 }
  
