2fa6200641093d5bdfae4eb5e80df03281aeb4ff
mattpannella <mattpannella@gmail.com>
2022-12-03 00:19:56 -0500

    woops. stupid array param

--- .github/test.php
+++ .github/test.php
@@ -33,7 +33,7 @@ function getReleaseDate($coreName)
     $file = "./Cores/{$coreName}/core.json";
     $json = file_get_contents($file);
 
-    $core = json_decode($json,true);
+    $core = json_decode($json);
     if(!empty($core->core->metadata->date_release)) {
         $d = $core->core->metadata->date_release;
         $date = date('Ymd', strtotime($d));
